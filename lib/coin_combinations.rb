9bbec9ea1370cae4addd37f52fe0b050f713ef9a
Kelsey Langlois and Luciano Oliveira <luciano@>
2017-09-07 08:59:58 -0700

    Update make_change to return correct coin ammounts for 1, 5, 10, and 25.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -3,7 +3,12 @@ class Integer
     if self < 0
       return "Error"
     elsif self == 0
-      return {"coins" => 0}
+      return {"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 0}
+    else
+      coin_values = {1 => "pennies", 5 => "nickels", 10 => "dimes", 25 => "quarters"}
+      change = {"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 0}
+      change.store(coin_values.fetch(self), 1)
+      return change
     end
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -7,6 +7,22 @@ describe("#make_change") do
   end
 
   it("returns 0 coins if number is 0") do
-    expect(0.make_change).to(eq({"coins" => 0}))
+    expect(0.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 0}))
+  end
+
+  it("returns 1 penny if number is 1") do
+    expect(1.make_change).to(eq({"pennies" => 1, "nickels" => 0, "dimes" => 0, "quarters" => 0}))
+  end
+
+  it("returns 1 nickel if number is 5") do
+    expect(5.make_change).to(eq({"pennies" => 0, "nickels" => 1, "dimes" => 0, "quarters" => 0}))
+  end
+
+  it("returns 1 dime if number is 10") do
+    expect(10.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 1, "quarters" => 0}))
+  end
+
+  it("returns 1 quarter if number is 25") do
+    expect(25.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 1}))
   end
 end
