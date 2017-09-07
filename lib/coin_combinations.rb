b61a1d36662b05d4904dccd157be6f6329d2d8f9
Kelsey Langlois and Luciano Oliveira <luciano@>
2017-09-07 09:19:51 -0700

    Update function to find combinations of coins.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -7,7 +7,14 @@ class Integer
     else
       coin_values = {1 => "pennies", 5 => "nickels", 10 => "dimes", 25 => "quarters"}
       change = {"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 0}
-      change.store(coin_values.fetch(self), 1)
+      values_array = coin_values.keys.sort.reverse
+      total = self
+      values_array.each do |value|
+        if total >= value
+          change.store(coin_values.fetch(value), total/value)
+          total -= (total/value) * value
+        end
+      end
       return change
     end
   end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -25,4 +25,20 @@ describe("#make_change") do
   it("returns 1 quarter if number is 25") do
     expect(25.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 1}))
   end
+
+  it("For numbers greater than 25, program returns number of quarters equal to input / 25 first") do
+    expect(50.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 0, "quarters" => 2}))
+  end
+
+  it("adds on number of dimes equal to (input - (input/25)) / 10 after number of quarters, for numbers not evenly divisible by 25") do
+    expect(70.make_change).to(eq({"pennies" => 0, "nickels" => 0, "dimes" => 2, "quarters" => 2}))
+  end
+
+  it("if number cannot be evenly made with quarters and dimes, adds on number of nickels needed to make number") do
+    expect(80.make_change).to(eq({"pennies" => 0, "nickels" => 1, "dimes" => 0, "quarters" => 3}))
+  end
+
+  it("if number cannot be evenly made with quarters, dimes, and nickels, adds on number of pennies needed to make number") do
+    expect(53.make_change).to(eq({"pennies" => 3, "nickels" => 0, "dimes" => 0, "quarters" => 2}))
+  end
 end
